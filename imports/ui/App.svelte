d5f6cbf25830fea128c10658908d5d112dfb92d0
Olivier Rousseau-Guyot <zeachco@gmail.com>
2022-08-30 23:09:58 -0400

    adds svelte components

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,22 +1,16 @@
 <script>
+  import User from "./User.svelte";
+  import Login from "./Login.svelte";
   let counter = 0;
   const addToCounter = () => {
     counter += 1;
-  }
+  };
 </script>
 
-
 <div class="container">
-  <h1>Welcome to Meteor!</h1>
-
+  <Login />
+  <a href="/about">About my site</a>
   <button on:click={addToCounter}>Click Me</button>
   <p>You've pressed the button {counter} times.</p>
-
-  <h2>Learn Meteor!</h2>
-  <ul>
-    <li><a href="https://svelte-tutorial.meteor.com/" target="_blank">Do the Tutorial</a></li>
-    <li><a href="http://guide.meteor.com" target="_blank">Follow the Guide</a></li>
-    <li><a href="https://docs.meteor.com" target="_blank">Read the Docs</a></li>
-    <li><a href="https://forums.meteor.com" target="_blank">Discussions</a></li>
-  </ul>
+  <User id={counter} />
 </div>
---
+++ imports/ui/User.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let id;
+  // setTimeout(() => (id += 3), 1000);
+</script>
+
+<div class="container">
+  <h1>Welcome to user # {id}!</h1>
+</div>
